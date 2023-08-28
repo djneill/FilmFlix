50dcc2eb32bd2ad0ff87ecda835c2a310ec72f13
DJ Neill <dj.neill856@gmail.com>
2023-08-28 14:58:33 -0500

    adding registration & login verification

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,8 @@
 
 import { useCallback, useState } from 'react'
+import axios from 'axios'
 import Input from '@/components/Input'
+import { signIn } from 'next-auth/react'
 
 const Auth = () => {
     
@@ -13,6 +15,31 @@ const Auth = () => {
     const toggleVariant = useCallback(() => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
     }, [])
+
+    const register = useCallback(async () => {
+        try {
+            await axios.post('/api/register', {
+                email,
+                name,
+                password
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }, [email, name, password]);
+
+    const login = useCallback(async () => {
+        try {
+            await signIn('credentials', {
+                email,
+                password,
+                redirect: false,
+                callbackUrl: '/'
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }, [email, password])
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -50,7 +77,7 @@ const Auth = () => {
                                 value={password}
                             />
                         </div>
-                        <button className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'>
+                        <button onClick={variant == 'login' ? login : register} className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'>
                             {variant === 'login' ? 'Login' : 'Sign up'}
                         </button>
                         <p className='text-neutral-500 mt-12'>
