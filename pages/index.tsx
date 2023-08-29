0cb66ad4a73ee3af7ee0b7430fe7a24b87317946
DJ Neill <dj.neill856@gmail.com>
2023-08-29 01:22:38 -0500

    deploy development version

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ export default function Home() {
       <h2 className="text-red-700 text-2xl p-4">A Netflix Clone</h2>
       <h3 className="text-red-700 text-xl p-4">Built with Next.js, React, Typescript, TailwindCSS, Next.js, MongoDB, NextAuth and Vercel</h3>
       <h4 className='text-red-700 text-xl p-4'>This is currently still in development. I&#39;m glad you&#39;re here, thanks for visiting and I&#39;m excited to show you what&#39;s to come</h4>
+      <h4 className='text-red-700 text-xl p-4'>You can view the Sign In / Register page here <a href='https://film-flix-opal.vercel.app/auth'>https://film-flix-opal.vercel.app/auth</a> </h4>
     </>
   )
 }
