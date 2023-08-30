816d4cfd0f682500f113f1345a0f267b5a89165f
DJ Neill <dj.neill856@gmail.com>
2023-08-30 03:33:48 -0500

    adding verification  for google safe browsing

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -55,18 +55,21 @@ const Auth = () => {
                 </nav>
                 <div className="flex justify-center">
                     <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+                        <p className="text-red-600 font-semibold mb-3">
+                            Do not use your real Netflix Password
+                        </p>
                         <h2 className="text-white text-4xl mb-8 font-semibold">
                             {variant === 'login' ? 'Sign In' : 'Register'}
                         </h2>
                         <div className="flex flex-col gap-4">
                             {variant === 'register' && (
                                 <Input
-                                label='Username'
-                                onChange={(ev: any) => setName(ev.target.value)}
-                                id='name'
-                                type='name'
-                                value={name}
-                            />
+                                    label='Username'
+                                    onChange={(ev: any) => setName(ev.target.value)}
+                                    id='name'
+                                    type='name'
+                                    value={name}
+                                />
                             )}
                             <Input
                                 label='Email'
