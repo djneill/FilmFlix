ea156d5fdb4c65dc0f1c29b0e997898c0334835a
DJ Neill <dj.neill856@gmail.com>
2023-09-04 14:29:17 -0500

    profile img src

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -32,7 +32,7 @@ const Profiles = () => {
                             
                             <div className='group flex-row w-44 mx-auto'>
                                 <div className='w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden'>
-                                    <img src='/images/default-profile.png' alt='profile' className='' />
+                                    <img src='public/images/default-blue.png' alt='profile' className='' />
                                 </div>
                                 <div className='mt-4 text-gray-400 text-2xl text-center group-hover:text-white'>
                                     {user?.name}
