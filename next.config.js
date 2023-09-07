25aae576bd9febc2a75ad15b82dc7435217d69d6
DJ Neill <dj.neill856@gmail.com>
2023-09-07 16:45:15 -0500

    add navbar components

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
 }
-// next.config.js
-module.exports = {
-  server: {
-    https: {
-      key: './localhost.key',
-      cert: './localhost.crt',
-    },
-  },
-};
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { NextPageContext } from 'next'
-import { getSession, signOut } from 'next-auth/react'
-import useCurrentUser from '@/hooks/useCurrentUser'
+import { getSession } from 'next-auth/react'
+import Navbar from '@/components/Navbar'
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context)
@@ -24,17 +23,9 @@ export async function getServerSideProps(context: NextPageContext) {
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const { data: user } = useCurrentUser()
-
   return (
     <>
-    <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout</button>
-    <p className="text-white">Logged in as: {user?.name}</p>
-      <h1 className="text-red-700 text-4xl p-4">FilmFlix</h1>
-      <h2 className="text-red-700 text-2xl p-4">A Netflix Clone</h2>
-      <h3 className="text-red-700 text-xl p-4">Built with React, Typescript, TailwindCSS, Next.js, MongoDB, NextAuth and Vercel</h3>
-      <h4 className='text-red-700 text-xl p-4'>This is currently still in development. I&#39;m glad you&#39;re here, thanks for visiting and I&#39;m excited to show you what&#39;s to come</h4>
-      <h4 className='text-red-700 text-xl p-4'>You can view the Sign In / Register page here <a href='https://film-flix-opal.vercel.app/auth'>https://film-flix-opal.vercel.app/auth</a> </h4>
+      <Navbar />
     </>
   )
 }
